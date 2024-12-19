90d22d743bc1f73b7ee9076182ed4681e54880e6
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-19 12:20:02 +0100

    Refactor Content intto seperate Part components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,16 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+      <Part part={props.part1} exercise={props.exercise1} />
     </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
   );
 };
 
