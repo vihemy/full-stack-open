893d3cdc0526794111d185dab85f5a1d39388033
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-12-19 11:46:35 +0100

    Replace boilerplate code

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercise1 = 10;
+  const part2 = "Using props to pass data";
+  const exercise2 = 7;
+  const part3 = "State of a component";
+  const exercise3 = 14;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercise1}
       </p>
-    </>
-  )
-}
+      <p>
+        {part2} {exercise2}
+      </p>
+      <p>
+        {part3} {exercise3}
+      </p>
+      <p>Number of exercices {exercise1 + exercise2 + exercise3}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
