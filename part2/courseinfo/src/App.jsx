6a39565a2c40f008a170bf6a996eda3e37dc14db
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-03 13:20:57 +0100

    Declare Course-component as seperate module
    2.5 Seperate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,4 @@
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      <b>total of {parts.reduce((s, p) => s + p.exercises, 0)} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <h2>{course.name}</h2>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
+import Course from "./components/Course";
 const App = () => {
   const courses = [
     {
