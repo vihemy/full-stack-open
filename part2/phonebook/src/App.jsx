60813e456f2a30779dba486f2cce16ed90e9f13a
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-03 14:50:41 +0100

    Implement addition of people to phonebook
    2.6: The Phonebook Step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Number = ({ person }) => {
+  return <p>{person.name}</p>;
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("Arto Hellas");
+
+  const addNumber = (event) => {
+    event.preventDefault();
+    const numberObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    };
+    setPersons(persons.concat(numberObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNumber}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <Number key={person.name} person={person} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
