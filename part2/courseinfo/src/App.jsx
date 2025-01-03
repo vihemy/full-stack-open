34580764e4c38bbe1d413fa87a0f0c5852c88fee
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-03 13:17:09 +0100

    Add support for multiple courses + remove header component
    2.4 Course information step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,7 +1,3 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
 const Part = ({ part }) => {
   return (
     <p>
@@ -31,7 +27,7 @@ const Total = ({ parts }) => {
 const Course = ({ course }) => {
   return (
     <div>
-      <Header name={course.name} />
+      <h2>{course.name}</h2>
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
@@ -39,29 +35,61 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <div>
+        {courses.map((course) => (
+          <Course course={course} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
