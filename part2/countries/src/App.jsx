2ff9413add086273292a91ba12e5b3e87041a0f0
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-07 21:58:23 +0100

    Fetch countries data from API and display countries that contains search field content

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Line = ({ country }) => {
+  return <p>{country.name.common}</p>;
+};
+
+const Result = ({ countries }) => {
+  return (
+    <div>
+      {countries.map((c) => (
+        <Line key={c.name.common} country={c} />
+      ))}
+    </div>
+  );
+};
 
 const SearchBar = ({ newSearch, handleSearchChange }) => {
   return (
     <div>
-      name: <input value={newSearch} onChange={handleSearchChange} />
+      find countries: <input value={newSearch} onChange={handleSearchChange} />
     </div>
   );
 };
 
 function App() {
+  const [countries, setCountries] = useState(null);
   const [newSearch, setNewSearch] = useState("");
 
-  const handleSearchChange = (event) => setNewSearch(event.target.value);
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => setCountries(response.data));
+  }, []);
+
+  if (!countries) return null;
+
+  const countriesToShow = countries.filter((c) =>
+    c.name.common.toLocaleLowerCase().includes(newSearch.toLocaleLowerCase())
+  );
+
+  const handleSearchChange = (event) => {
+    event.preventDefault();
+    setNewSearch(event.target.value);
+  };
 
   return (
     <>
@@ -19,6 +50,7 @@ function App() {
         newSearch={newSearch}
         handleSearchChange={handleSearchChange}
       />
+      <Result countries={countriesToShow} />
     </>
   );
 }
