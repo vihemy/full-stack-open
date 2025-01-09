8c5ced2fc8fa2f66a537e09cdbbdffa3a94ad1eb
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-09 10:31:47 +0100

    Extract Result-component into folder

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,60 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-
-const SingleLineResult = ({ countryName }) => {
-  return <p>{countryName}</p>;
-};
-
-const FullResult = ({ country }) => {
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>capital: {country.capital}</p>
-      <p>area: {country.area}</p>
-      <p>
-        <b>languages:</b>
-        {console.log(country.languages)}
-      </p>
-      <ul>
-        {Object.values(country.languages).map((l) => (
-          <li key={l}>{l}</li>
-        ))}
-      </ul>
-      <img
-        src={country.flags.svg}
-        alt={country.flags.alt}
-        width={150}
-        height={150}
-      />
-    </div>
-  );
-};
-
-const Result = ({ filteredCountries }) => {
-  let content;
-
-  switch (true) {
-    case !filteredCountries.length:
-      content = <div>No country with this name.</div>;
-      break;
-    case filteredCountries.length > 10:
-      content = <div>Too many matches, specify another filter</div>;
-      break;
-    case filteredCountries.length > 1:
-      content = (
-        <div>
-          {filteredCountries.map((c) => (
-            <SingleLineResult key={c.name.common} countryName={c.name.common} />
-          ))}
-        </div>
-      );
-      break;
-    default:
-      content = <FullResult country={filteredCountries[0]} />;
-  }
-
-  return <div>{content}</div>;
-};
+import Result from "./components/Result";
 
 const SearchBar = ({ newSearch, handleSearchChange }) => {
   return (
