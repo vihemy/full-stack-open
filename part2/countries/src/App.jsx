d38e7d616da607138efc1aee130705eeadf5898f
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-09 10:28:51 +0100

    Display queried country data
    2.18*: Data for countries, sted 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,18 +1,59 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const Line = ({ country }) => {
-  return <p>{country.name.common}</p>;
+const SingleLineResult = ({ countryName }) => {
+  return <p>{countryName}</p>;
 };
 
-const Result = ({ countries }) => {
+const FullResult = ({ country }) => {
   return (
     <div>
-      {countries.map((c) => (
-        <Line key={c.name.common} country={c} />
-      ))}
+      <h1>{country.name.common}</h1>
+      <p>capital: {country.capital}</p>
+      <p>area: {country.area}</p>
+      <p>
+        <b>languages:</b>
+        {console.log(country.languages)}
+      </p>
+      <ul>
+        {Object.values(country.languages).map((l) => (
+          <li key={l}>{l}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flags.svg}
+        alt={country.flags.alt}
+        width={150}
+        height={150}
+      />
     </div>
   );
+};
+
+const Result = ({ filteredCountries }) => {
+  let content;
+
+  switch (true) {
+    case !filteredCountries.length:
+      content = <div>No country with this name.</div>;
+      break;
+    case filteredCountries.length > 10:
+      content = <div>Too many matches, specify another filter</div>;
+      break;
+    case filteredCountries.length > 1:
+      content = (
+        <div>
+          {filteredCountries.map((c) => (
+            <SingleLineResult key={c.name.common} countryName={c.name.common} />
+          ))}
+        </div>
+      );
+      break;
+    default:
+      content = <FullResult country={filteredCountries[0]} />;
+  }
+
+  return <div>{content}</div>;
 };
 
 const SearchBar = ({ newSearch, handleSearchChange }) => {
@@ -50,7 +91,7 @@ function App() {
         newSearch={newSearch}
         handleSearchChange={handleSearchChange}
       />
-      <Result countries={countriesToShow} />
+      <Result filteredCountries={countriesToShow} />
     </>
   );
 }
