3c1796bad85955af0dbb6d77592114fd47df9f4b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-13 21:34:48 +0100

    Add custom logging for POST requests
    3.8*: Phonebook backend step 8

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
