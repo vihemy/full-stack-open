fcaa80b14fa4a7b8bf171eea09a161192bb27129
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-13 10:38:33 +0100

    Add route to get all persons
    3.1: Phonebook backend step 1

--- part3/index.js
+++ part3/index.js
@@ -24,6 +24,10 @@ let persons = [
   },
 ];
 
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
