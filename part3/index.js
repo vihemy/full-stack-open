4d9ac673cd75ed3d5b954e3c195b1490c5b04e30
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-15 10:45:23 +0100

    Deploy app
    3.10 Phonebook backend step 10

--- part3/index.js
+++ part3/index.js
@@ -9,6 +9,7 @@ morgan.token("body", function (req, res) {
 const app = express();
 
 app.use(cors());
+app.use(express.static('dist'))
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -101,6 +102,6 @@ app.post("/api/persons/", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
