0ec16c5adf78011e298e3ddf9fdc696ade3f5254
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-17 08:19:30 +0100

    Setup delete handler with database
    3.15: Phonebook database, step 3

--- part3/index.js
+++ part3/index.js
@@ -65,9 +65,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
