79b7f5a7059f1ca2556fe5dbfe72f29cef7b9cdf
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-17 11:33:17 +0100

    Add validation to POST and PUT
    3.19*: Phonebook database, step 7

--- part3/index.js
+++ part3/index.js
@@ -55,7 +55,11 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: body.number,
   };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -72,17 +76,6 @@ app.delete("/api/persons/:id", (request, response, next) => {
 
 app.post("/api/persons/", (request, response, next) => {
   const body = request.body;
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name missing",
-    });
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: "number missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
@@ -107,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
