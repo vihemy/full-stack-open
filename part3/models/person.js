23dfb94b4055d1a6e51c99873236ae4bc79ed5cf
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-17 13:07:15 +0100

    Add custom validation to number
    3.20*: Phonebook database, step 8

--- part3/models/person.js
+++ part3/models/person.js
@@ -15,7 +15,18 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true },
-  number: { type: String, required: true },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (number) {
+        return /\d{2,3}-\d{6,}$/.test(number);
+      },
+      message: () =>
+        `"Number must consist of two or three digits + " - " + six or more digits"`,
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
