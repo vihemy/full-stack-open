6148ba0af8ff211d17629e684adb5e0185b2237f
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-21 08:57:39 +0100

    Add function and unit test for finding favorite blog
    4.5*: Helper Functions and Unit Tests, step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, post) => total + post.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined
+  return blogs.reduce((maxObj, currentObj) => {
+    return currentObj.likes > maxObj.likes ? currentObj : maxObj
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
