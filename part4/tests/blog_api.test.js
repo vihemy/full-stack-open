50a71f004c5ad39da8c21e3d18a9cc1b67aeef13
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-01-22 21:03:33 +0100

    Add api tests

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,27 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('there are a correct amount of blogs ', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
