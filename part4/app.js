9764c1402596e36ea2775598628f3c50615ff05b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-04 12:29:27 +0100

    Include users-controller in app
    4.15: Blog List Expansion, step 3

--- part4/app.js
+++ part4/app.js
@@ -1,4 +1,5 @@
 const blogsRouter = require ('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
@@ -23,5 +24,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
