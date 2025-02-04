042cde58c225d7dced83bc2a7ce913f64330b35a
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-04 12:04:30 +0100

    Add missing parameter to .catch

--- part4/app.js
+++ part4/app.js
@@ -15,7 +15,7 @@ mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
-  .catch(() => {
+  .catch((error) => {
     logger.error('error connecting to MongoDB', error.message)
   })
 
