9ee54fe406009b2df441cb686b6f23610766be53
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-05 12:16:44 +0100

    Add blog reference to userSchema

--- part4/models/user.js
+++ part4/models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  blogs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog'
+  }],
   username: {
     type: String,
     required: true,
