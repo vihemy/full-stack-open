b237b2a5b7bb634f0ed23661ccdbfcbf69887ea5
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-05 12:42:07 +0100

    Require token auth to POST request
    4.19: Blog List Expansion, step 7

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -12,6 +21,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id){
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+
   if (!body.title || !body.url) {
     return response.status(400).end()
   }
@@ -19,7 +35,7 @@ blogsRouter.post('/', async (request, response) => {
     body.likes = 0
   }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
