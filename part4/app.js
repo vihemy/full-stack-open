89a9c8890253a664b33304d6744ef04713e7d9ff
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-05 12:25:42 +0100

    Add login router to app
    4.18: Blog List Expansion, step 6

--- part4/app.js
+++ part4/app.js
@@ -1,5 +1,6 @@
 const blogsRouter = require ('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const express = require('express')
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
