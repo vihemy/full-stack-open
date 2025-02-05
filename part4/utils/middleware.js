44d11955e6f1bbc5b4e2091b81c4c49672179ba5
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-05 13:40:15 +0100

    Specify errorhandling in middleware

---
+++ part4/utils/middleware.js
@@ -0,0 +1,24 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  errorHandler
+}
