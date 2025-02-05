49bc0335c0fe5236f99eb687e00c1caab4ffb0f9
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-05 12:17:05 +0100

    Populate user with blog post data
    4.17: Blog List Expansion, step 5

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -22,6 +22,7 @@ usersRouter.post('/', async (request, response) => {
 usersRouter.get('/', async (requests, response) => {
   const users = await User
     .find({})
+    .populate('blogs', { url: 1, title: 1, author: 1 })
 
   response.json(users)
 })
