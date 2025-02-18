9f1c5f1c3c00b013389a4f947abf9170abac2ffd
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-18 20:16:09 +0100

    Clean request.body for user population in PUT route
    5.8: Blog List Frontend, step 8

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -58,8 +58,9 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+  const body = request.body
+  delete body.user;
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, { new: true })
   response.status(200).json(updatedBlog)
 })
 
