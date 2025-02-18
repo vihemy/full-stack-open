35fb3b8cf6d71be0a03516998359beaa1ab56b31
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-02-18 19:29:25 +0100

    Add details-toggle to blog component
    5.7 Blog List Frontend, step 7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
+import { useState } from "react"
+
+const BlogDetails = ({ blog }) => (
   <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+    {blog.url}
+    <div>
+      likes {blog.likes} <button onClick={null}>like</button>
+    </div>
+    {blog.user.name}
+  </div>
+);
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    border: '1px solid black',
+    marginBottom: '5px'
+  }
+
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+      {showDetails && <BlogDetails blog={blog} />}
+    </div>
+  );
+};
 
 export default Blog
