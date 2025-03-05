6be6772edfb9a4b5442a0cd0cd3c873aa775115c
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-05 20:53:53 +0100

    Check if details are rendered when suitable
    5.14: Blog List Tests, step 2

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,19 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
+import { expect, test } from 'vitest';
+
+const blog = {
+  title: 'test6',
+  author: '12345678',
+  url: 'www.test.dk',
+  likes: 8,
+  user: {
+    username: "Tiger",
+    name: "Tigram Hamasalan"
+  }
+};
+
+const userObject = {
+  username: "Tiger",
+  name: "Tigram Hamasalan"
+};
 
 test('renders title & author, not url & likes', () => {
-  const blog = {
-    title: 'test6',
-    author: '12345678',
-    url: 'www.test.dk',
-    likes: 8
-  };
-
   const { container } = render(<Blog blog={blog} />);
 
-  const div = container.querySelector('.blog');
-  expect(div).toHaveTextContent('test6');
-  expect(div).toHaveTextContent('12345678');
-  expect(div).not.toHaveTextContent('www.test.dk');
-  expect(div).not.toHaveTextContent('likes: 8');
+  const blogDiv = container.querySelector('.blog');
+  expect(blogDiv).toHaveTextContent('test6');
+  expect(blogDiv).toHaveTextContent('12345678');
+  expect(blogDiv).not.toHaveTextContent('www.test.dk');
+  expect(blogDiv).not.toHaveTextContent('likes 8');
+});
+
+test('url & likes are shown when "view"-button is pressed', async () => {
+  const { container } = render(<Blog blog={blog} user={userObject} />);
+
+  const user = userEvent.setup();
+  const viewButton = screen.getByText('view');
+  await user.click(viewButton);
+
+  const blogDiv = container.querySelector('.blog');
+  expect(blogDiv).toHaveTextContent('www.test.dk');
+  expect(blogDiv).toHaveTextContent('likes 8');
 });
