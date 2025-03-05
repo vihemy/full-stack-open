022cd85ec7c0b4a46c211d5cb8fbca61e9f6e6d9
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-05 21:03:13 +0100

    Test if like button calls event handler
    5.15: Blog List Tests, step 3

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 
 const blog = {
   title: 'test6',
@@ -40,3 +40,19 @@ test('url & likes are shown when "view"-button is pressed', async () => {
   expect(blogDiv).toHaveTextContent('www.test.dk');
   expect(blogDiv).toHaveTextContent('likes 8');
 });
+
+test('clicking the button twice calls event handler twice', async () => {
+  const mockHandler = vi.fn();
+
+  render(<Blog blog={blog} user={userObject} handleLike={mockHandler} />);
+
+  const user = userEvent.setup();
+  const viewButton = screen.getByText('view');
+  await user.click(viewButton);
+  
+  const likeButton = screen.getByText('like');
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(mockHandler.mock.calls).toHaveLength(2);
+})
