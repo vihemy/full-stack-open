0e82aef6c85d07fe4be9c5de68f3c05202a8844d
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-05 21:25:11 +0100

    Test that blogForm calls eventhandler with correct details
    5.16: Blog List Tests, step 4

---
+++ part5/src/components/BlogForm.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import BlogForm from './BlogForm';
+import userEvent from '@testing-library/user-event';
+import { expect, vi } from 'vitest';
+
+test('blogForm calls eventhandler with correct details', async() => {
+const user = userEvent.setup();
+const createBlog = vi.fn()
+const notify = vi.fn()
+
+render(<BlogForm createBlog={createBlog} notify={notify} />)
+
+const titleInput = screen.getByPlaceholderText('title');
+const authorInput = screen.getByPlaceholderText('author');
+const urlInput = screen.getByPlaceholderText('url');
+
+const submitButton = screen.getByText('create');
+
+await user.type(titleInput, 'testing title input')
+await user.type(authorInput, 'testing author input')
+await user.type(urlInput, 'testing url input')
+
+await user.click(submitButton)
+
+expect(createBlog.mock.calls).toHaveLength(1)
+expect(createBlog.mock.calls[0][0].title).toBe('testing title input') 
+expect(createBlog.mock.calls[0][0].author).toBe('testing author input') 
+expect(createBlog.mock.calls[0][0].url).toBe('testing url input') 
+})
