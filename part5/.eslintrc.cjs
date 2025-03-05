549bbc9a7670158fbebc0dde3ccf562c2aed505b
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-05 12:02:04 +0100

    Add vitest-globals

--- part5/.eslintrc.cjs
+++ part5/.eslintrc.cjs
@@ -1,55 +1,57 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      es2020: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react/jsx-runtime',
-      'plugin:react-hooks/recommended',
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    "vitest-globals/env": true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:vitest-globals/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    "indent": [
+      "error",
+      2  
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh'],
-    rules: {
-      "indent": [
-        "error",
-        2  
-      ],
-      "linebreak-style": [
-        "error",
-        "unix"
-      ],
-      "quotes": [
-        "error",
-        "single"
-      ],
-      "semi": [
-        "error",
-        "always"
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-        "error", "always"
-      ],
-      "arrow-spacing": [
-        "error", { "before": true, "after": true }
-      ],
-      "no-console": 0,
-      "react/prop-types": 0,
-      "react/react-in-jsx-scope": "off",
-      "no-unused-vars": 0
-    },
-    overrides: [
-      {
-        files: ['**/*.jsx'], // Ensure ESLint looks at .jsx files
-        rules: {
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": 0
+  },
+  overrides: [
+    {
+      files: ['**/*.jsx'], // Ensure ESLint looks at .jsx files
+      rules: {
 
-        },
       },
-    ],
-  }
+    },
+  ],
+}
