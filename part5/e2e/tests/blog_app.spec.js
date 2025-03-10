7d3703cd5f0fae94fa8d64ef6a71b813d2b36255
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-10 13:33:07 +0100

    Test if login form is shown
    5.17: Blog List End To End Testing, step 1

---
+++ part5/e2e/tests/blog_app.spec.js
@@ -0,0 +1,12 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('log in to application')).toBeVisible()
+    await expect(page.locator('.login-form')).toBeVisible()
+  })
+})
