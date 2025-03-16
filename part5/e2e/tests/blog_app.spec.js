af28aa3a3f39a47add752128f8abae68a62ea7c2
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-16 10:10:59 +0100

    Test succesfull and failed login.
    5.18: Blog List End To End Testing, step 2

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ request, page }) => {
@@ -16,5 +17,19 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('log in to application')).toBeVisible()
     await expect(page.locator('.login-form')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'Tiger', "wrongpassword")
+      const errorDiv = await page.locator('.notification')
+      await expect(errorDiv).toContainText('wrong username or password')
+      expect(page.getByText('Tigram Hamasalan logged in')).not.toBeVisible()
+    })
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'Tiger', "abc123")
+      await expect(page.getByText('Tigram Hamasalan logged in')).toBeVisible()
+    })
   })
 })
