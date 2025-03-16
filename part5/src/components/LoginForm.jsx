9974d1517ca8c24b5952f2f095bbf8a8da2c1012
Victor Hempel-Mydtskov <victor@mydtskov.com>
2025-03-16 10:10:34 +0100

    Add id's for testing

--- part5/src/components/LoginForm.jsx
+++ part5/src/components/LoginForm.jsx
@@ -3,6 +3,7 @@ const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }
     <div>
         username
       <input
+        data-testid='username'
         type="text"
         value={username}
         name="Username"
@@ -12,6 +13,7 @@ const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }
     <div>
         password
       <input
+        data-testid='password'
         type="password"
         value={password}
         name="Password"
